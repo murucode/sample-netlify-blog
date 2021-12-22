abc574965e33464c87e44369a48b526eebe3bb79
Muru <murugesh.vish@gmail.com>
2021-12-22 11:46:26 +0530

    updated main text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Served from netlify
         </p>
         <a
           className="App-link"
